24dc31ae9fde1ecec678c7995f10a6b3c5b8d494
GarrettF <grfoster24@gmail.com>
2022-02-05 12:08:07 -0500

    checking again

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const mongoose = require("mongoose")
 const PORT = process.env.PORT || 3001
 
+mongoose.connect(
+  "mongodb+srv://GarrettF:<password>@practicecluster.jw23b.mongodb.net/cookbookDatabase?retryWrites=true&w=majority"
+)
+
 app.listen(PORT, () => {
   console.log("Server running successfully")
 })
