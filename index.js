9deb0f2b76719d58e6d070c81b920cbac37c91e0
GarrettF <grfoster24@gmail.com>
2022-02-05 12:07:26 -0500

    trying to hide index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const PORT = process.env.PORT || 3001
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("Server running successfully")
 })
